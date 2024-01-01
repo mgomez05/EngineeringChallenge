0809a464c4ba4e423430788d5c6d09873231caf0
mgomez05 <gomezruss7@gmail.com>
2024-01-01 01:06:31 -0500

    Added text inputs to the registration screen in index.ts and centered the title

--- native-app/app/index.tsx
+++ native-app/app/index.tsx
@@ -1,12 +1,12 @@
-import { Button } from 'react-native';
+import { Button, TextInput } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebase';
 
 export default function RegisterScreen() {
-  const [email, setEmail] = useState('practice@user.com');
-  const [password, setPassword] = useState('practicepassword');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   const createUser = async () => {
     await createUserWithEmailAndPassword(auth, email, password)
@@ -25,9 +25,22 @@ export default function RegisterScreen() {
       });
   };
 
+  console.log('Current email and password:', email, ',', password);
+
   return (
     <View>
-      <Text>Register Screen</Text>
+      <Text style={{ textAlign: 'center' }}>Register Screen</Text>
+      <TextInput
+        value={email}
+        placeholder={'Username'}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        value={password}
+        placeholder={'Password'}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
       <Button title='Register' onPress={createUser} />
     </View>
   );

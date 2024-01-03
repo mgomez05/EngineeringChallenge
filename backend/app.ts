643d5cb7313f50f9d08badbd820bccc9d0be2afc
mgomez05 <gomezruss7@gmail.com>
2024-01-02 22:13:49 -0500

    Added endpoint at POST /machine-health using the new machine database insertion logic

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,6 @@
-import express, {Request, Response} from 'express';
-import {getMachineHealth} from './machineHealth';
+import express, { Request, Response } from 'express';
+import { getMachineHealth } from './machineHealth';
+import { insertMachineDataToDatabase } from './addMachineHealthToDatabase';
 
 const app = express();
 const port = 3001;
@@ -8,9 +9,20 @@ const port = 3001;
 app.use(express.json());
 
 // Endpoint to get machine health score
+// app.post('/machine-health', (req: Request, res: Response) => {
+//   const result = getMachineHealth(req);
+//   if (result.error) {
+//     res.status(400).json(result);
+//   } else {
+//     res.json(result);
+//   }
+// });
+
+// Endpoint to insert machine health data into database
 app.post('/machine-health', (req: Request, res: Response) => {
-  const result = getMachineHealth(req);
-  if (result.error) {
+  console.log('In app in POST /machine-health');
+  const result = insertMachineDataToDatabase(req);
+  if (!result) {
     res.status(400).json(result);
   } else {
     res.json(result);

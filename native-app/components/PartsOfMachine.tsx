8f3075cd83c429f6107cddc5ec09a50e0dee1ef7
mgomez05 <gomezruss7@gmail.com>
2024-01-05 02:15:23 -0500

    Updated PartsOfMachine.tsx to not show the 'id' and 'dateCreated' fields of the machine

--- native-app/components/PartsOfMachine.tsx
+++ native-app/components/PartsOfMachine.tsx
@@ -1,15 +1,15 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {Button, Platform, StyleSheet, TextInput} from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Platform, StyleSheet, TextInput } from 'react-native';
 
-import {Text, View} from './Themed';
+import { Text, View } from './Themed';
 import axios from 'axios';
 import Constants from 'expo-constants';
 import RNPickerSelect from 'react-native-picker-select';
 import machineData from '../data/machineData.json';
-import {MachineType} from '../data/types';
+import { MachineType } from '../data/types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useMachineData} from '../app/useMachineData';
-import {useFocusEffect} from 'expo-router';
+import { useMachineData } from '../app/useMachineData';
+import { useFocusEffect } from 'expo-router';
 
 export const PartsOfMachine = ({
   machineName,
@@ -23,11 +23,15 @@ export const PartsOfMachine = ({
       {parts && (
         <>
           <Text style={styles.title}>{machineName}</Text>
-          {Object.keys(parts).map((key) => (
-            <Text key={key}>
-              {key}: {parts[key]}
-            </Text>
-          ))}
+          {Object.keys(parts).map((key) => {
+            if (key !== 'dateCreated' && key !== 'id') {
+              return (
+                <Text key={key}>
+                  {key}: {parts[key]}
+                </Text>
+              );
+            }
+          })}
         </>
       )}
     </>

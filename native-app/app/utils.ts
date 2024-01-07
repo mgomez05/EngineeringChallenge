f3a90147ebdd21e8962f60e2c73271112b4a25c8
mgomez05 <gomezruss7@gmail.com>
2024-01-07 13:05:08 -0500

    Added comments to app/utils.ts for clarity

--- native-app/app/utils.ts
+++ native-app/app/utils.ts
@@ -1,7 +1,12 @@
 import { Platform } from 'react-native';
 
+// Initialize the api url with a default url
 let apiUrl: string =
   'https://fancy-dolphin-65b07b.netlify.app/api/machine-health';
+
+// Returns the api url for local development if this app is run in development
+//   - The url returned depends on whether the platform is android or ios
+// Otherwise, returns the default url, shown above
 export const getApiUrl = () => {
   if (__DEV__) {
     apiUrl = `http://${

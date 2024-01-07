c55ddd9ddbbec73565eca1afc9b80fad0a13fed5
mgomez05 <gomezruss7@gmail.com>
2024-01-07 01:40:52 -0500

    Cleaned up the comments in editMachineData.ts

--- backend/editMachineData.ts
+++ backend/editMachineData.ts
@@ -31,8 +31,7 @@ type MachinePart =
 export const editMachineData = async (req: Request) => {
   const machineId = req.body.machineId;
 
-  // Add the machine as a new machine to the database if no
-  // machine id was provided
+  // If no machine id was provided, insert the data as a new machine to the database
   if (!machineId) {
     req.body = {
       machines: req.body.machine,
@@ -52,7 +51,8 @@ export const editMachineData = async (req: Request) => {
     };
   }
 
-  // Parse the request body into a <machines> record
+  // Otherwise, if a machineId was provided, we attempt to update an existing machine
+  // - First, we parse request body into a <machines> record
   const {
     machine,
   }: {
@@ -67,14 +67,15 @@ export const editMachineData = async (req: Request) => {
     };
   }
 
+  // Then, extract the machine's data from the <machine> records
+  // and use that data to update it in the database
   try {
-    // Get the one machine in the array
     for (const machineName in machine) {
       const machineType = machineName as MachineType;
       const machineParts = machine[machineType] as Record<MachinePart, string>;
 
       // Check if a machine with a matching <machineType> and <machineId>
-      // exists in the databse
+      // exists in the database
       let machineRecordInDatabase;
       if (machineType === MachineType.AssemblyLine) {
         machineRecordInDatabase = await getPrismaClient().assemblyLine.findMany(
@@ -100,6 +101,7 @@ export const editMachineData = async (req: Request) => {
             where: { id: machineId },
           });
       } else {
+        // If the <machineType> wasn't recognized, log an error and return a 400 Bad Request response
         console.error(
           'Error: Unknown machine type:',
           machineType,
@@ -111,7 +113,8 @@ export const editMachineData = async (req: Request) => {
         };
       }
 
-      // Return an error response if no matching machine was found in the database
+      // If the <machineType> was recognized, but no matching machine was found in the database,
+      // return a 400 Bad Request response
       if (!machineRecordInDatabase || machineRecordInDatabase.length !== 1) {
         return {
           status: 400,
@@ -120,7 +123,7 @@ export const editMachineData = async (req: Request) => {
       }
 
       // Collect all the machine's parts and values into a JS object
-      // we can use to update the machine
+      // we can use to update the machine in the database
       let partsToUpdate = {};
       for (const part in machineParts) {
         if (machineParts.hasOwnProperty(part)) {
@@ -163,13 +166,14 @@ export const editMachineData = async (req: Request) => {
       };
     }
 
-    // If we get here, that means the 'for' loop never ran, so return an error message
+    // If we get to this line, that means the 'for' loop never ran, so return an error message
     return {
       status: 400,
       error:
-        'Insufficient information provided to update machine, please reformat your request to have one machine with one corresponding part to update',
+        'Insufficient information provided to update machine, please reformat your request to have one machine, and one corresponding machine part to update',
     };
   } catch (error) {
+    // If an unexpected error occurred, log an error and return a 500 Server Error Response
     console.error(
       'There was an error updating the machine in the database, error',
       error

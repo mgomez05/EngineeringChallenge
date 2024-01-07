0570925fcf4892134cc058b88d853e23bd304b09
mgomez05 <gomezruss7@gmail.com>
2024-01-06 23:22:31 -0500

    Added a new function editMachineData() to the newUseMachineData hook for communicating with the new PUT /machine endpoint

--- native-app/app/newUseMachineData.ts
+++ native-app/app/newUseMachineData.ts
@@ -44,9 +44,44 @@ export const newUseMachineData = () => {
     await loadMachineData();
   };
 
+  // If <machineId> is the empty string:
+  //    - Returns true if the machine was created
+  //    - Otherwise returns false
+  // If <machineId> is non-empty:
+  //    - Returns true if the machine was updated
+  //    - Otherwise returns false
+  const editMachineData = async (machineId: string, machineData: any) => {
+    try {
+      const apiUrl = getApiUrl();
+      const response = await axios.put(`${apiUrl}/machine`, {
+        machineId: machineId,
+        machine: machineData,
+      });
+
+      // If the request succeeded, reload the machine data and return true
+      // Otherwise, return false
+      if (response.status === 200) {
+        console.log('Machine was created / updated successfully');
+
+        await loadMachineData();
+        return true;
+      } else {
+        console.error(
+          'There was an error creating / updating the machine data, status code was',
+          response.status
+        );
+        return false;
+      }
+    } catch (error) {
+      console.error('Failed to edit machine', error);
+      return false;
+    }
+  };
+
   return {
     machineData,
     loadMachineData,
     resetMachineData,
+    editMachineData,
   };
 };
